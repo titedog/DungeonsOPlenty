996bc5b255263e742e63db4da3e2c2868ff4034e
qorple <79393251+qorple@users.noreply.github.com>
2021-03-16 08:55:26 -0700

    Create ItemRegistry.java

---
+++ src/main/java/net/qorple/dungeonsoplenty/registry/ItemRegistry.java
@@ -0,0 +1,22 @@
+package registry;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.network.packet.s2c.play.CombatEventS2CPacket;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
+import qorple.dungeonsoplenty.DungeonsOPlenty;
+
+public class ItemRegistry {
+
+   
+
+    public static void initItems() {
+        
+    }
+}
